a015fed0c259f48aafe1f6d123e3ec0d951d15b9
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-07-20 22:00:44 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
+use std::io;
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+async fn process(mut socket: TcpStream) {
+    let mut buf = [0; 512];
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-
-                let mut buffer = [0; 512];
-
-                loop {
-                    let count = stream.read(&mut buffer).unwrap();
-                    if count == 0 {
-                        break;
-                    }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    loop {
+        let count = socket.read(&mut buf).await.unwrap();
+        if count == 0 {
+            break;
         }
+        socket.write(b"+PONG\r\n").await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
